78a11fef00298350a0cf20712fff9327cc678d29
Miguel Ferre <miguelferreporcel@gmail.com>
2023-03-30 21:30:20 +0200

    signIn + jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -37,5 +37,19 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-  res.json('signIn')
+  const userFound = await User.findOne({email: req.body.email}).populate('roles')
+
+  if(!userFound) return res.status(400).json({message: 'User not found'})
+  
+  const comparePassword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword)
+  }
+  const matchPassword = await comparePassword(req.body.password, userFound.password)
+  if(!matchPassword) return res.status(401).json({token: null, message: 'Invalid password'})
+
+  const token = jwt.sign({id: userFound._id}, JWT_SECRET, {
+    expiresIn: 86400 // 24 horas
+  })
+  console.log(userFound)
+  res.json({token})
 }
